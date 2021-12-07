65680a60f5ab2129cad02938b1569e93f87c2d7e
nkongstad <niko7131@gmail.com>
2021-12-07 17:29:38 +0100

    htmldag

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include("./functions.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webshop</title>
+</head>
+
+<body>
+    <div class="menu">
+        <div class="container">
+            <a href="https://farstrup.dk/">FARSTRUP FURNITURE</a>
+
+            <nav>
+                <ul>
+                    <li><a href="#">Kollektion</a></li>
+                    <li><a href="#">Forhandlere</a></li>
+                    <li><a href="#">Om os</a></li>
+                    <li><a href="#">Kontakt</a></li>
+                    <li><a href="#">Contract</a></li>
+                    <li><a href="#">Kurv</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+    </div>
+<hr>
+    <div class="productBox">
+        <img src="billeder\Munin-04-highres_compressed-e1632735048593.jpg" alt="Munin 6360" style="width100%">
+        <h1>Munin 6360</h1>
+        <p class="price">36.455 Kr.</p>
+        <p><button>Køb nu</button></p>
+    </div>
+
+
+</body>
+
+</html>
